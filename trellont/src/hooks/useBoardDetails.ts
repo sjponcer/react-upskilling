23d9a8d7e59f967e4dbb8e59b96d30cb7b98fa00
francopagnotta <francoapagnotta@gmail.com>
2025-12-15 12:35:55 -0300

    create hook to fetch board details and cards by board

---
+++ trellont/src/hooks/useBoardDetails.ts
@@ -0,0 +1,41 @@
+import { useQuery } from "@tanstack/react-query";
+import {
+  fetchBoardDetails,
+  fetchCardsByBoard,
+  type BoardDetails,
+  type Card,
+} from "../services/api";
+
+export const useBoardDetails = (boardId: string | undefined) => {
+  const boardQuery = useQuery<BoardDetails, Error>({
+    queryKey: ["board", boardId],
+    queryFn: () => fetchBoardDetails(boardId!),
+  });
+
+  const cardsQuery = useQuery<Card[], Error>({
+    queryKey: ["cards", boardId],
+    queryFn: () => fetchCardsByBoard(boardId!),
+  });
+
+  const organizedCards = cardsQuery.data
+    ? {
+        todo: cardsQuery.data.filter((c) => c.status === "todo"),
+        "in-progress": cardsQuery.data.filter(
+          (c) => c.status === "in-progress"
+        ),
+        done: cardsQuery.data.filter((c) => c.status === "done"),
+      }
+    : {
+        todo: [] as Card[],
+        "in-progress": [] as Card[],
+        done: [] as Card[],
+      };
+
+  return {
+    board: boardQuery.data,
+    cards: organizedCards,
+    isLoading: boardQuery.isLoading || cardsQuery.isLoading,
+    isError: boardQuery.isError || cardsQuery.isError,
+    error: boardQuery.error || cardsQuery.error,
+  };
+};
