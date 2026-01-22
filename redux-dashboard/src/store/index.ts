e55d82117b565ee08e451b6eca5c51ae356a0e7f
francopagnotta <francoapagnotta@gmail.com>
2026-01-22 15:00:28 -0300

    add logsReducer and loggerMiddleware in store configuration

--- redux-dashboard/src/store/index.ts
+++ redux-dashboard/src/store/index.ts
@@ -1,12 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import itemsReducer from './itemsSlice';
 import notificationsReducer from './notificationsSlice';
+import logsReducer from './logsSlice';
+import { loggerMiddleware } from './loggerMiddleware';
 
 export const store = configureStore({
   reducer: {
     items: itemsReducer,
     notifications: notificationsReducer,
+    logs: logsReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(loggerMiddleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
